9268ef86fef19affa7cc84df9d52b12f5b70719c
ffmm <masamail4210@gmail.com>
2015-07-21 18:06:58 +0900

    １章

--- PracticeOne_B.java
+++ PracticeOne_B.java
@@ -1,24 +1,10 @@
 public class PracticeOne_B {
 
-	public static void main(String[] args) {
+	String concatPath(String folder, String file) {
+		if (!folder.endsWith("\\"))
+			folder += "\\";
 
-		String file = "readme.txt";
-		String folder = "c:\\user\\";
-
-		StringBuilder sb = new StringBuilder();
-
-		if (folder.endsWith("\\")) {
-			sb.append(folder);
-			sb.append(file);
-		} else {
-			sb.append(folder);
-			sb.append("\\");
-			sb.append(file);
-		}
-		String s = sb.toString();
-
-		System.out.println(s);
+		return folder + file;
 
 	}
-
 }
